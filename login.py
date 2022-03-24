4fc02d408b46cf11cb8b850acc7b6f2db31e3ea4
Vaishaal Shankar <vaishaal@berkeley.edu>
2022-03-24 09:22:27 -0700

    fixed login stuff

--- login.py
+++ login.py
@@ -1,28 +1,28 @@
 import webbrowser
 import streamlit as st
 import sqlite3
-import main_app
 
-def app():
+def login():
+    st.session_state.login = True
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    st.session_state['name'] = None
-    with st.form("Login"):
-        email = st.text_input("E-mail")
-        password = st.text_input("Password", type="password")
-        c.execute("SELECT * FROM LOGIN WHERE email = ? AND password = ?", (email, password))
-        button = st.form_submit_button("Login")
+    login_form = st.form("Login")
+    email = login_form.text_input("E-mail")
+    password = login_form.text_input("Password", type="password")
+    login_button = login_form.form_submit_button("submit")
+    print("potato", email,password,login_button)
+    if login_button is not None:
         user = []
+        c.execute("SELECT * FROM LOGIN WHERE email = ? AND password = ?", (email, password))
         user = c.fetchall()
         if user:
             st.session_state['name'] =user[0][1]
+            print("WOOO SUCCESS")
             authenticated = True
             st.success("Login Successful")
             st.write(st.session_state['name'])
-            if button:
-                main_app.PAGES["home"]
         else:
             st.error("Login Failed")
 
 if __name__ == "__main__":
-    app()
+    login()
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import sqlite3
 import webbrowser
+from login import login
 
 
-
-def app():
+def signup_page():
     db = sqlite3.connect('data.db')
     conn = db.cursor()
     st.title("Sign Up")
     st.write("Enter the following information")
+
     with st.form('Sign-Up'):
         first_name = st.text_input("Enter your first name")
         last_name = st.text_input("Enter your last name")
@@ -20,22 +21,28 @@ def app():
         st.checkbox("Are you a Teacher/Faculty")
 
         button = st.form_submit_button("Submit")
-        if button:
-            st.session.first_name = first_name
-            webbrowser.open_new_tab("http://localhost:8503")
+    if button is not None:
+        if email != "":
+            try:
+                conn.execute('INSERT INTO LOGIN VALUES (?,?,?,?)', (email, first_name, last_name, password))
+                db.commit()
+                db.close()
+                st.write("Thank you for your submission")
+            except sqlite3.IntegrityError:
+                st.error("This user already exists")
+def app():
+    if 'login' not in st.session_state:
+        st.session_state['login'] = False
+
+    placeholder = st.empty()
+    with placeholder.container():
+        signup_page()
 
     login_button = st.button("Already have an account? Login.")
-    if login_button:
-        webbrowser.open_new_tab("http://localhost:8502")
+    if login_button or st.session_state['login']:
+        placeholder.empty()
+        login()
 
-    if email != "":
-        try:
-            conn.execute('INSERT INTO LOGIN VALUES (?,?,?,?)', (email, first_name, last_name, password))
-            db.commit()
-            db.close()
-            st.write("Thank you for your submission")
-        except sqlite3.IntegrityError:
-            st.error("This user already exists")
 
 
 if __name__ == "__main__":
