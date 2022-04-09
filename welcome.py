9e6c3847c5f5a73d2aa0245c4d33a4e4666056b9
Vishan Shankar <vishannshankar@gmail.com>
2022-04-09 11:24:06 +0530

    added graphs to admin panel, output for some subjects work.

--- welcome.py
+++ welcome.py
@@ -12,9 +12,9 @@ def app():
         st.write("What subjects would you be interested in taking (Choose up to 3 in each)")
         hl_options = st.multiselect('Higher Level Subjects',["Math", "English", "Psychology", "Economics", "Business Management", "History", "Spanish", "French", "Hindi", "Tamil", "Physics", "Biology", "Chemistry", "Computer Science", "Environmental Science", "Theatre", "Visual Arts"])
         sl_options = st.multiselect('Standard Level Subjects',["Math", "English", "Psychology", "Economics", "Business Management", "History", "Spanish", "French", "Hindi", "Tamil", "Physics", "Biology", "Chemistry", "Computer Science", "Environmental Science", "Theatre", "Visual Arts"])
-        subject_conf = st.checkbox("I don't know what subjects I want to take in IB")
+        subject_conf = st.checkbox("I am sure that I want to take these courses")
         career_path_options = st.selectbox('Career Paths',["Accounting", "Business", "Engineering", "Law", "Medicine", "Nursing", "Science", "Social Sciences", "Education", "Other"])
-        career_conf = st.checkbox("I don't know what career path I want to pursue")
+        career_conf = st.checkbox("I am sure about my Career Choice")
         st.write("Our System will give you suggestions as well as send your information to a counsellor to help you get a better idea about how to go about your IB journey")
         submit = st.form_submit_button("Submit")
         if submit:
